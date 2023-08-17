0a64da930eda0be2569fca208a81099c90b5cd93
ethan-id <ehancock1648@gmail.com>
2023-08-16 22:04:09 -0500

    Add API

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
 	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+		<section className="flex items-center text-6xl justify-center">
 			Hey! This is gonna be an app for my Flask API!
 		</section>
 	);
