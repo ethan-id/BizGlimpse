98956ba2853e552e397365b05c0031a3b2a9aae2
ehancock <ehancock@iastate.edu>
2024-03-07 17:48:23 -0600

    Update site index to not be /home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,26 +2,13 @@
 
 import {Grabber} from '@/app/home/components/grabber';
 import {MyParticles} from '@/app/home/components/particles';
-import {Button, ButtonGroup} from '@nextui-org/react';
-import {signIn, useSession} from 'next-auth/react';
 
 export default function Home() {
-	const { data: session } = useSession();
-
-	console.log(session);
 
 	return (
-		<section className=''>
-			{session ? <>
-				<MyParticles/>
-				<Grabber/>
-			</> : (
-				<ButtonGroup className='flex justify-center items-center h-[80vh]'>
-					<Button onClick={() => signIn('github')}>
-						Sign in with GitHub
-					</Button>
-				</ButtonGroup>
-			)}
+		<section>
+			<MyParticles/>
+			<Grabber/>
 		</section>
 	);
 }
