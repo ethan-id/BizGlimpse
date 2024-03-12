0ecef45fd43f5a661411821532ae8b4d17ffe8c9
ehancock <ehancock@iastate.edu>
2024-03-12 00:14:54 -0500

    Update SignIn Page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,12 +3,9 @@
 import {HomePage} from '@/app/home/components/HomePage';
 import {MyParticles} from '@/app/home/components/particles';
 
-export default function Home() {
-
-	return (
-		<section>
-			<MyParticles/>
-			<HomePage/>
-		</section>
-	);
-}
+export const Home = () => (
+	<section>
+		<MyParticles/>
+		<HomePage/>
+	</section>
+);
